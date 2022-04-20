3d1feea4fefeb3a5e5042fa7ceb4b98992e14dd3
Jeffrey <jpgl.vantillo@student.avans.nl>
2022-04-20 16:36:10 +0200

    added RESTful functionalities

--- node-server.js
+++ node-server.js
@@ -1,18 +1,67 @@
-const http = require('http')
-
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
-const result = {
-  code: 200,
-  message: 'OK!',
-};
+let database = [];
+let id = 0;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(JSON.stringify(result));
+app.use(bodyParser.json());
+
+app.get('*', (req, res, next) => {
+  const method = req.method
+  console.log(`Method ${method} called`);
+  next();
 });
 
-server.listen(port, () => {
+app.get('api/movie', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: database,
+  });
+});
+
+app.post('/api/movie', (req, res) => {
+  let movie = req.body;
+  console.log(movie);
+  id++;
+
+  movie = {
+    id,
+    ...movie,
+  };
+  database.push(movie);
+  console.log(database);
+  res.status(201).json({
+    status: 201,
+    result: movie,
+  })
+});
+
+app.get('/api/movie/:movieId', (req, res) => {
+  const movieId = req.params.movieId
+  let movie = database.filter((item) => item.id == movieId);
+  if(movie.length > 0){
+    res.status(200).json({
+      status: 200,
+      result: movie,
+    });
+    console.log(movie);
+  }else{
+    res.status(404).json({
+      status: 404,
+      result: `Movie with ID ${movieId} was not found.`,
+    });
+  }
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: 'End-point not found!',
+  });
+});
+
+app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
 })
